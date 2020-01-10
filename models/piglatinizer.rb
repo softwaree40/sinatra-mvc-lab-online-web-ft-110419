5e4e6072fe786390f80916afc03d606c56472363
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-10 01:02:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,24 @@
 require "pry"
 class PigLatinizer
     def piglatinize(word)
+<<<<<<< HEAD
         words = word.strip
+=======
+        word = word.strip
+>>>>>>> 9237e08a117d1a2805aec6de91faa2667ade27e5
         vowels = "aeiou"
         consonat = "bcdfghjklmnpqrstvwxyz"
         before_array = []
         after_array =[]
+<<<<<<< HEAD
         word_array = []
+=======
+>>>>>>> 9237e08a117d1a2805aec6de91faa2667ade27e5
     if vowels.include?(word[0].downcase)
        return (word + "way")
+    
     end
+<<<<<<< HEAD
     
      words = word.split(" ")
     
@@ -27,6 +36,18 @@ class PigLatinizer
         before_array.clear
      end
      sentence = word_array.join(" ")
+=======
+     word.each_char.with_index do |char,idx|
+     if vowels.include?(char)
+        after_array << word[idx..-1]
+        break
+      else
+        before_array << char
+       end
+       
+    end
+      new_join = after_array.join + before_array.join + "ay"
+>>>>>>> 9237e08a117d1a2805aec6de91faa2667ade27e5
    end
    
 end
