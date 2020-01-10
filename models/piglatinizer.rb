a97e76aa2d910e59e33a06334fa6d6e10f168063
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-10 01:24:05 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,32 +1,22 @@
-require "pry"
 class PigLatinizer
-    def piglatinize(word)
-        words = word.strip
-        vowels = "aeiou"
-        consonat = "bcdfghjklmnpqrstvwxyz"
-        before_array = []
-        after_array =[]
-        word_array = []
-    if vowels.include?(word[0].downcase)
-       return (word + "way")
+
+  def piglatinize(text)
+
+    words = text.split(" ")
+    converted_words = words.collect do |word|
+      if is_vowel?(word[0])
+        word = word + "way"
+      else
+        first_vowel_index = word.index(/[aeiou]/)
+        word = "#{word.slice!(0...first_vowel_index)}ay".prepend(word)
+      end
+
+      word
     end
-    
-     words = word.split(" ")
-    
-    words.each do |word|
-       word.each_char.with_index do |char,idx|
-         if vowels.include?(char)
-            after_array << word[idx..-1]
-            break
-          else
-            before_array << char
-           end
-       end
-        word_array << after_array.join + before_array.join + "ay"
-        after_array.clear
-        before_array.clear
-     end
-     sentence = word_array.join(" ")
-   end
-   
+    converted_words.join(" ")
+  end
+
+  def is_vowel?(char)
+    char.downcase.match?(/[aeiou]/)
+  end
 end
