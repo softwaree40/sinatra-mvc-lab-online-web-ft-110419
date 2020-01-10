e6ca1f58fb4730e7f32510f820930d8ef0b160da
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-10 01:04:08 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,53 +1,22 @@
-require "pry"
 class PigLatinizer
-    def piglatinize(word)
-<<<<<<< HEAD
-        words = word.strip
-=======
-        word = word.strip
->>>>>>> 9237e08a117d1a2805aec6de91faa2667ade27e5
-        vowels = "aeiou"
-        consonat = "bcdfghjklmnpqrstvwxyz"
-        before_array = []
-        after_array =[]
-<<<<<<< HEAD
-        word_array = []
-=======
->>>>>>> 9237e08a117d1a2805aec6de91faa2667ade27e5
-    if vowels.include?(word[0].downcase)
-       return (word + "way")
-    
-    end
-<<<<<<< HEAD
-    
-     words = word.split(" ")
-    
-    words.each do |word|
-       word.each_char.with_index do |char,idx|
-         if vowels.include?(char)
-            after_array << word[idx..-1]
-            break
-          else
-            before_array << char
-           end
-       end
-        word_array << after_array.join + before_array.join + "ay"
-        after_array.clear
-        before_array.clear
-     end
-     sentence = word_array.join(" ")
-=======
-     word.each_char.with_index do |char,idx|
-     if vowels.include?(char)
-        after_array << word[idx..-1]
-        break
+
+  def piglatinize(text)
+
+    words = text.split(" ")
+    converted_words = words.collect do |word|
+      if is_vowel?(word[0])
+        word = word + "way"
       else
-        before_array << char
-       end
-       
+        first_vowel_index = word.index(/[aeiou]/)
+        word = "#{word.slice!(0...first_vowel_index)}ay".prepend(word)
+      end
+
+      word
     end
-      new_join = after_array.join + before_array.join + "ay"
->>>>>>> 9237e08a117d1a2805aec6de91faa2667ade27e5
-   end
-   
+    converted_words.join(" ")
+  end
+
+  def is_vowel?(char)
+    char.downcase.match?(/[aeiou]/)
+  end
 end
